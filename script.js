cd48875774225d6c1ef8182368c0ea02360947e5
yuvrajsinghgit <yuvrajsingh.emailid@gmail.com>
2025-03-16 12:58:35 +0530

    changed UI

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendValue(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
